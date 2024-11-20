05ac3dcc6fee2e571eed4e6f2fed6760bcd4f1e4
95xervit <erikvillarreal9505@gmail.com>
2024-11-19 20:08:51 -0500

    Update navbar

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -12,88 +12,89 @@ export default function Header() {
     console.log(window.location.href)
     router.push(path);
   };
+
+  const handleMenuItemClick = (action: () => void) => {
+    action();            // Ejecuta la acción del item
+    setIsMenuOpen(false); // Cierra el menú
+  };
     
   const menuItems = [
     { name: "Adopciones", action: () => donationNavigation('/#adopciones') },
-    { name: "Tienda Gora", url: "https://gorafundacion.org" },
-    { name: "Contáctanos", url: "#contactanos" },
+    { name: "Tienda Gora", action: () => window.open("https://gorafundacion.org", "_blank") },
+    { name: "Contáctanos", action: () => donationNavigation('#contactanos') },
     { name: "Ayudanos", action: () => donationNavigation('/Home/Donations') }, 
   ];
 
-    
   return (
     <div>
-        <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-greenGora justify-between md:py-4">
+        <Navbar 
+            onMenuOpenChange={setIsMenuOpen} 
+            isMenuOpen={isMenuOpen} 
+            className="bg-greenGora justify-between md:py-4"
+        >
             <NavbarContent>
                 <NavbarBrand>
                     <Link href="/" className="flex items-center">
-                        <Image src="/LogoGora.png" width="140" height="40" alt="Logo Gora"/>
+                    <Image src="/LogoGora.png" width="140" height="40" alt="Logo Gora" />
                     </Link>
                 </NavbarBrand>
-                
+
+                {/* Botón hamburguesa */}
                 <NavbarMenuToggle
                     aria-label={isMenuOpen ? "Close menu" : "Open menu"}
                     className="lg:hidden text-OrangeLightGora"
                 />
             </NavbarContent>
-            {/* menu web */}
+
+            {/* Menú web */}
             <NavbarContent className="hidden lg:flex gap-8" justify="start">
                 <NavbarItem>
                     <Link 
-                        className="text-lg text-pinkLightGora hover:text-orangeGora hover:underline hover:underline-offset-8" 
-                        onClick={() =>
-                            {
-                                window.location.pathname === "/Home/Donations" ? router.push('/#adopciones') : router.push('/#adopciones')
-                            }
-                        }>
+                        className="text-lg text-pinkLightGora hover:text-orangeGora hover:underline hover:underline-offset-8 cursor-pointer"
+                        onClick={() => router.push('/#adopciones')}
+                    >
                         Adopciones
                     </Link>
                 </NavbarItem>
-                <NavbarItem >
-                    <Link className="text-lg text-pinkLightGora hover:text-orangeGora hover:underline hover:underline-offset-8" href="https://gorafundacion.org" target="_blank">
+                <NavbarItem>
+                    <Link 
+                        className="text-lg text-pinkLightGora hover:text-orangeGora hover:underline hover:underline-offset-8 cursor-pointer" 
+                        href="https://gorafundacion.org" 
+                        target="_blank"
+                    >
                         Tienda Gora
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link className="text-lg text-pinkLightGora hover:text-orangeGora hover:underline hover:underline-offset-8" href="#">
+                    <Link 
+                        className="text-lg text-pinkLightGora hover:text-orangeGora hover:underline hover:underline-offset-8 cursor-pointer" 
+                        href="#"
+                    >
                         Contáctanos
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
-                    {/* Redirección a Donations usando onClick y router */}
                     <Link 
-                        className="text-lg text-pinkLightGora hover:text-orangeGora hover:underline hover:underline-offset-8" 
-                        onClick={() => { 
-                                console.log(window.location.pathname) 
-                                router.push('/Home/Donations')
-                        }} 
-                        style={{ cursor: 'pointer' }}>
-                        Ayudanos
+                        className="text-lg text-pinkLightGora hover:text-orangeGora hover:underline hover:underline-offset-8 cursor-pointer" 
+                        onClick={() => router.push('/Home/Donations')}
+                    >
+                        Ayúdanos
                     </Link>
                 </NavbarItem>
             </NavbarContent>
-            {/* menu cel */}
+
+            {/* Menú móvil */}
             <NavbarMenu>
-                {menuItems.map((item, index) => (
+            {menuItems.map((item, index) => (
                 <NavbarMenuItem key={`${item.name}-${index}`} className="mt-4">
-                    {item.url ? (
-                        <Link
-                            className="flex justify-center py-2 w-full text-4xl text-greenGora hover:text-orangeGora"
-                            href={item.url}
-                        >
-                            {item.name}
-                        </Link>
-                    ) : (
-                        <Link
-                            className="flex justify-center py-2 w-full text-4xl text-greenGora hover:text-orangeGora"
-                            onClick={item.action}  // Usa la acción de redirección interna
-                            style={{ cursor: 'pointer' }}
-                        >
-                            {item.name}
-                        </Link>
-                    )}
-                 </NavbarMenuItem>
-                ))}
+                <Link
+                    className="flex justify-center py-2 w-full text-4xl text-greenGora hover:text-orangeGora cursor-pointer"
+                    onClick={() => handleMenuItemClick(item.action)}
+                >
+                    {item.name}
+                </Link>
+                </NavbarMenuItem>
+            ))}
             </NavbarMenu>
         </Navbar>
     </div>
