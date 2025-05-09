5620435f1872a9a6f72e10f98a35623bf87e3d03
95xervit <erikvillarreal9505@gmail.com>
2025-05-08 22:24:27 -0500

    Web en mantenimiento

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,32 +2,105 @@ import './globals.css'
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.css';
 import 'primeicons/primeicons.css';
-import 'primeflex/themes/primeone-light.css'  
-import { Poppins } from 'next/font/google'
+import 'primeflex/themes/primeone-light.css';
+import { Link, Image } from "@nextui-org/react"
+import { Poppins } from 'next/font/google';
 import { Metadata } from 'next';
+import DogsWork from '@/public/DogsWork.png'
 import ConfigureAmplifyClientSide from './amplify-cognito-config';
-const popinsFont = Poppins({ weight:['400', '600', '700'], subsets: ['latin'] })
+
+const popinsFont = Poppins({ weight: ['400', '600', '700'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Adopciones Canadog',
-  description: 'Adopciones'
-}
+  description: 'Adopciones',
+};
+
+// Activar y desactivar web
+const ENABLED = false;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  if (!ENABLED) {
+    return (
+      <html lang="es">
+        <body className="flex flex-col h-screen items-center justify-center mn:gap-6 md:gap-10">
+          <div className="flex flex-col bg-white justify-center items-center text-center">
+            <Image 
+              alt="Dogs Work" 
+              src={DogsWork.src}
+              height={500} 
+              width={400} 
+              className="mn:w-44 mn:h-60 xl:w-[600px] xl:h-[500px]"
+            />
+            <h1 className="text-greenCanadog mn:text-xl md:text-4xl xl:text-5xl font-semibold mb-4">
+              🚧 Estamos en mantenimiento 🚧
+            </h1>
+            <p className="text-blackCanadog mn:text-sm md:text-3xl">
+              Estamos trabajando para mejorar tu experiencia. Vuelve pronto...
+            </p>
+          </div>
+
+          <div className='flex flex-wrap flex-col gap-6 text-center'>
+            <p className="text-blackCanadog mn:text-sm md:text-2xl items-center px-4">
+              Te gustaria adoptar? contáctanos via whatsapp o visitanos en instagram para estar al tanto de nuestro refugio.
+            </p>
+
+            <div className="flex mn:flex-col md:flex-row justify-center items-center gap-4"> 
+              <Link 
+                href='https://wa.me/+573104684683' 
+                target='_blank'
+              >
+                <div className='flex justify-center items-center gap-2'>
+                  <Image 
+                      alt="lovePets" 
+                      src="/LogoWhatsapp.png" 
+                      height={40} 
+                      width={40} 
+                      className="mn:h-8 mn:w-8 xl:h-10 xl:w-10 hover:scale-110 "
+                  />
+                  <p className="text-greenCanadog mn:text-lg lg:text-2xl">
+                    +57 3104684683
+                  </p>
+                </div>
+              </Link> 
+
+              <a 
+                href="https://www.instagram.com/canadog_store/" 
+                target='_blank' 
+                className="flex justify-center items-center space-x-1"
+              >
+                <svg x="0px" y="0px" width="40" height="40" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24" className="text-[#E1306C] hover:text-[#e1306bd9] hover:scale-110 mn:p-1 md:p-0">
+                  <path 
+                    fillRule="evenodd" 
+                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" 
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="text-greenCanadog mn:text-lg lg:text-2xl">
+                  canadog_store
+                </span>
+              </a> 
+            </div>
+          </div>
+        </body>
+      </html>
+    );
+  }
+
   return (
-    <html className='h-full' id="id" lang="es">
+    <html className="h-full" lang="es">
       <head>
-        {
-          /*<meta name="google-adsense-account" content="ca-pub-9189120606062802"></meta>*/
-          }
-        <script async={true} data-cfasync="false" src="//pl26062928.effectiveratecpm.com/fe98476a7a70879b195eafa963b474de/invoke.js"></script>
+        {/* <meta name="google-adsense-account" content="ca-pub-9189120606062802" /> */}
+        <script
+          async={true}
+          data-cfasync="false"
+          src="//pl26062928.effectiveratecpm.com/fe98476a7a70879b195eafa963b474de/invoke.js"
+        ></script>
       </head>
       <body className={popinsFont.className}>
-        <>
-          <ConfigureAmplifyClientSide/>
-          {children}
-        </>
+        <ConfigureAmplifyClientSide />
+        {children}
       </body>
     </html>
-  )
+  );
 }
